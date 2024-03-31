6c8c308565c5c750e57d1296a36540bca7e1647f
dkaszyns <01169181@pw.edu.pl>
2024-03-31 23:06:44 +0200

    Add integration test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
     id("java")
+    idea
 }
 
 group = "com.tkom"
 version = "1.0-SNAPSHOT"
+
+sourceSets {
+    create("intTest") {
+        compileClasspath += sourceSets.main.get().output
+        runtimeClasspath += sourceSets.main.get().output
+    }
+}
+
+val intTestImplementation: Configuration by configurations.getting {
+    extendsFrom(configurations.implementation.get())
+}
 
 repositories {
     mavenCentral()
@@ -13,6 +25,10 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.assertj:assertj-core:3.25.3")
+
+    intTestImplementation(platform("org.junit:junit-bom:5.9.1"))
+    intTestImplementation("org.junit.jupiter:junit-jupiter")
+    intTestImplementation("org.assertj:assertj-core:3.25.3")
 }
 
 tasks.test {
